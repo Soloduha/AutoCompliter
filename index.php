6fbca2fb48045ce3cc5d3b08208c78d37b9cccd9
soloduhavlad <soloduhavlad@A24COMP6>
2018-12-11 16:44:52 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Автосервіс</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur delectus, deleniti doloremque
+    doloribus ex expedita nemo odio officia quia. Aspernatur aut blanditiis culpa cumque cupiditate dolorum
+    excepturi ipsum quo.</p>
+
+    <div class="dropList">
+        <input type="text"
+               name="userInput"
+               id="userInput"
+                class="custom-select"
+                placeholder="search..."/>
+        <div id="droplistContent" class="dropdown-content">
+            <a href="#">aaaaa</a>
+            <a href="#">bbbbbb</a>
+            <a href="#">ccccc</a>
+            <a href="#">qqqq</a>
+            <a href="#">www</a>
+        </div>
+        <div id="droplistContent" class="dropdown-content">
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur delectus, deleniti doloremque
+            doloribus ex expedita nemo odio officia quia. Aspernatur aut blanditiis culpa cumque cupiditate dolorum
+            excepturi ipsum quo.</p>
+    </div>
+</body>
+</html>
---
+++ js/autocompliter.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',initSelect);
+
+let str="aaaaa,bbbbbb,ccccc,qqqq,www";
+let arrItems = str.split(",");
+
+function initSelect(){
+    let input = document.getElementById("userInput");
+    for(let i=0;i<arrItems.length;i++)
+    {
+
+    }
+}
